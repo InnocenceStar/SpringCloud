493416cba57499c499f622eee7925d6653f8e9ee
Innocence <x.y.2014@qq.com>
2021-07-02 11:12:18 +0800

    dashboard相关 http://localhost:9001/hystrix/ http://localhost:8008/hystrix.stream

--- cloud-provider-hystrix-payment8008/src/main/java/com/qaq/springcloud/PaymentMain8008.java
+++ cloud-provider-hystrix-payment8008/src/main/java/com/qaq/springcloud/PaymentMain8008.java
@@ -1,9 +1,12 @@
 package com.qaq.springcloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -11,5 +14,20 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class PaymentMain8008 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8008.class, args);
+    }
+
+    /**
+     * 此配置为服务监控配置，和服务容错本身无关，springcloud升级后的坑
+     *ServletRegistrationBean因为springboot的默认路径不是"/hystrix.stream"
+     * 配置完成后即可
+     */
+    @Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet hystrixMetricsStreamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(hystrixMetricsStreamServlet);
+        servletRegistrationBean.setLoadOnStartup(1);
+        servletRegistrationBean.addUrlMappings("/hystrix.stream");
+        servletRegistrationBean.setName("HystrixMetricsStreamServlet");
+        return servletRegistrationBean;
     }
 }
