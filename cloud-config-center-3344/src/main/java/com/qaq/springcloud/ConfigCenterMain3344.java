7b34a378d176cb7477f203f7921ec5df66a04232
Innocence <x.y.2014@qq.com>
2021-07-07 19:06:27 +0800

    bus

--- cloud-config-center-3344/src/main/java/com/qaq/springcloud/ConfigCenterMain3344.java
+++ cloud-config-center-3344/src/main/java/com/qaq/springcloud/ConfigCenterMain3344.java
@@ -3,7 +3,14 @@ package com.qaq.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+//这是一个配置中心
+
 //http://config-3344.com:3344/master/application-dev.yml
+//对所有客户端的刷新
+//刷新内容,广播通知>curl -X POST "http://localhost:3344/actuator/bus-refresh".
+//刷新内容,定点通知>curl -X POST "http://localhost:3344/actuator/bus-refresh/config-client:3355".(config-client:3355 微服务名称：端口号)
+@RefreshScope
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigCenterMain3344 {
--- cloud-config-client-3366/src/main/java/com/qaq/springcloud/controller/ConfigClientController.java
+++ cloud-config-client-3366/src/main/java/com/qaq/springcloud/controller/ConfigClientController.java
@@ -5,15 +5,19 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RefreshScope //刷新内容,>curl -X POST "http://localhost:3355/actuator/refresh".
+@RefreshScope //刷新内容,>curl -X POST "http://localhost:3366/actuator/refresh".
 @RestController
 public class ConfigClientController {
+
+    @Value("${server.port}")
+    private String serverPort;
 
     @Value("${config.info}")
     private String configInfo;
 
     @GetMapping("/configInfo")
     public String getConfigInfo() {
-        return configInfo;
+        return "port: "+serverPort+" config info:"+configInfo;
     }
+
 }
