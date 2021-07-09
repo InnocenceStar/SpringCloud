f678a96f6620d7da174c84ea7fe123c1e0ba955a
Innocence <x.y.2014@qq.com>
2021-07-09 18:23:06 +0800

    StreamMQ

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/qaq/springcloud/StreamMQConsumerMain8803.java
@@ -0,0 +1,12 @@
+package com.qaq.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class StreamMQConsumerMain8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQConsumerMain8803.class, args);
+    }
+}
